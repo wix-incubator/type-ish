72a485c62cbb6b7bde990565c163cb2f633471b9
Daniel Zlotin <danielzl@wix.com>
2017-08-01 14:36:46 +0300

    refactor

--- src/contract.js
+++ src/contract.js
@@ -1,57 +1,46 @@
 const _ = require('lodash');
 
-class TypeClass {
-  constructor({ params, validateFn }) {
-    this.validateFn = validateFn;
-    this.validate = this.validate.bind(this);
-    this.params = params;
-  }
-  validate(instance) {
-    this.validateFn(instance, ...this.params);
-  }
-  extend(extendingValidateFn) {
-    return Type((...p) => {
-      this.validateFn(...p);
-      extendingValidateFn(...p);
-    });
-  }
-}
-
 function Type(validateFn) {
-  return (...params) => new TypeClass({ params, validateFn });
+  return (...params) => new TypeValidator({ params, validateFn });
 }
 
 const StringType = Type((instance) => {
-  if (!_.isString(instance)) {
-    throw new Error(`expected ${instance} to be a StringType`);
-  }
+  assert(
+    _.isString(instance),
+    `expected ${instance} to be a StringType`
+  );
 });
 
 const NumberType = Type((instance) => {
-  if (!_.isNumber(instance)) {
-    throw new Error(`expected ${instance} to be a NumberType`);
-  }
+  assert(
+    _.isNumber(instance),
+    `expected ${instance} to be a NumberType`
+  );
 });
 
 const BooleanType = Type((instance) => {
-  if (!_.isBoolean(instance)) {
-    throw new Error(`expected ${instance} to be a BooleanType`);
-  }
+  assert(
+    _.isBoolean(instance),
+    `expected ${instance} to be a BooleanType`
+  );
 });
 
 const ArrayType = Type((instance, valueType) => {
-  if (!_.isArray(instance)) {
-    throw new Error();
-  }
+  assert(
+    _.isArray(instance),
+    `expected ${instance} to be a ArrayType`
+  );
+
   _.forEach(instance, (element) => {
     valueType.validate(element);
   });
 });
 
 const MapType = Type((instance, scheme) => {
-  if (!_.isPlainObject(instance)) {
-    throw new Error(`expected a plain object`);
-  }
+  assert(
+    _.isPlainObject(instance),
+    `expected ${instance} to be a MapType`
+  );
 
   const cloned = _.cloneDeep(instance);
 
@@ -60,23 +49,30 @@ const MapType = Type((instance, scheme) => {
     _.unset(cloned, key);
   });
 
-  if (!_.isEmpty(cloned)) {
-    throw new Error('expected exact keys');
-  }
+  assert(
+    _.isEmpty(cloned),
+    'expected exact keys'
+  );
 });
 
 const Optional = Type((instance, valueType) => {
-  if (_.isUndefined(instance)) {
-    return;
+  if (!_.isUndefined(instance)) {
+    valueType.validate(instance);
   }
-  valueType.validate(instance);
 });
 
 const EnumType = Type((instance, ...enums) => {
-  if (!_.includes(enums, instance)) {
-    throw new Error(`expected one of ${enums}`);
-  }
+  assert(
+    _.includes(enums, instance),
+    `expected one of ${enums}`
+  );
 });
+
+function assert(what, msg) {
+  if (!what) {
+    throw new Error(msg);
+  }
+}
 
 module.exports = {
   Type,
@@ -88,3 +84,23 @@ module.exports = {
   EnumType,
   Optional
 };
+
+class TypeValidator {
+  constructor({ params, validateFn }) {
+    this.validateFn = validateFn;
+    this.validate = this.validate.bind(this);
+    this.params = params;
+  }
+
+  validate(instance) {
+    this.validateFn(instance, ...this.params);
+  }
+
+  extend(extendingValidateFn) {
+    return Type((...p) => {
+      this.validateFn(...p);
+      extendingValidateFn(...p);
+    });
+  }
+}
+
--- src/contract.test.js
+++ src/contract.test.js
@@ -6,29 +6,29 @@ describe('contract tests', () => {
   it('StringType', () => {
     expect(() => StringType().validate('hello')).not.toThrow();
     expect(() => StringType().validate('')).not.toThrow();
-    expect(() => StringType().validate()).toThrowError(`expected undefined to be a StringType`);
-    expect(() => StringType().validate(null)).toThrowError(`expected null to be a StringType`);
-    expect(() => StringType().validate(123)).toThrowError(`expected 123 to be a StringType`);
+    expect(() => StringType().validate()).toThrow(`expected undefined to be a StringType`);
+    expect(() => StringType().validate(null)).toThrow(`expected null to be a StringType`);
+    expect(() => StringType().validate(123)).toThrow(`expected 123 to be a StringType`);
   });
 
   it('NumberType', () => {
     expect(() => NumberType().validate(123)).not.toThrow();
     expect(() => NumberType().validate(-123.456)).not.toThrow();
-    expect(() => NumberType().validate()).toThrowError(`expected undefined to be a NumberType`);
-    expect(() => NumberType().validate(null)).toThrowError(`expected null to be a NumberType`);
-    expect(() => NumberType().validate('123')).toThrowError(`expected 123 to be a NumberType`);
+    expect(() => NumberType().validate()).toThrow(`expected undefined to be a NumberType`);
+    expect(() => NumberType().validate(null)).toThrow(`expected null to be a NumberType`);
+    expect(() => NumberType().validate('123')).toThrow(`expected 123 to be a NumberType`);
   });
 
   it('BooleanType', () => {
-    expect(() => BooleanType().validate()).toThrowError(`expected undefined to be a BooleanType`);
+    expect(() => BooleanType().validate()).toThrow(`expected undefined to be a BooleanType`);
     expect(() => BooleanType().validate(null)).toThrow();
     expect(() => BooleanType().validate(false)).not.toThrow();
   });
 
   it('ArrayType', () => {
     expect(() => ArrayType(NumberType()).validate([1, 2, 3])).not.toThrow();
-    expect(() => ArrayType(NumberType()).validate('hello')).toThrowError();
-    expect(() => ArrayType(NumberType()).validate(['hi'])).toThrowError();
+    expect(() => ArrayType(NumberType()).validate('hello')).toThrow('expected hello to be a ArrayType');
+    expect(() => ArrayType(NumberType()).validate(['hi'])).toThrow('expected hi to be a NumberType');
   });
 
   it('custom type: MinMaxNumberType', () => {
@@ -43,8 +43,8 @@ describe('contract tests', () => {
 
     expect(() => MinMaxNumberType(10, 20).validate()).toThrow();
     expect(() => MinMaxNumberType(10, 20).validate(15)).not.toThrow();
-    expect(() => MinMaxNumberType(10, 20).validate(8)).toThrowError(`8 out of range`);
-    expect(() => MinMaxNumberType(10, 20).validate(22)).toThrowError(`22 out of range`);
+    expect(() => MinMaxNumberType(10, 20).validate(8)).toThrow(`8 out of range`);
+    expect(() => MinMaxNumberType(10, 20).validate(22)).toThrow(`22 out of range`);
   });
 
   it('MapType', () => {
@@ -53,14 +53,14 @@ describe('contract tests', () => {
       age: NumberType()
     });
 
-    expect(() => mapType.validate()).toThrowError();
-    expect(() => mapType.validate('hello')).toThrowError();
-    expect(() => mapType.validate(['hi'])).toThrowError();
+    expect(() => mapType.validate()).toThrow();
+    expect(() => mapType.validate('hello')).toThrow();
+    expect(() => mapType.validate(['hi'])).toThrow();
     class A { }
-    expect(() => mapType.validate(new A())).toThrowError(`expected a plain object`);
-    expect(() => mapType.validate({})).toThrowError('expected undefined to be a StringType');
-    expect(() => mapType.validate({ name: 123 })).toThrowError('expected 123 to be a StringType');
-    expect(() => mapType.validate({ name: 'the name', age: 'ageless' })).toThrowError('expected ageless to be a NumberType');
+    expect(() => mapType.validate(new A())).toThrow(`expected [object Object] to be a MapType`);
+    expect(() => mapType.validate({})).toThrow('expected undefined to be a StringType');
+    expect(() => mapType.validate({ name: 123 })).toThrow('expected 123 to be a StringType');
+    expect(() => mapType.validate({ name: 'the name', age: 'ageless' })).toThrow('expected ageless to be a NumberType');
     expect(() => mapType.validate({ name: 'the name', age: 123 })).not.toThrow();
 
     const transitiveMapType = MapType({
@@ -80,7 +80,7 @@ describe('contract tests', () => {
       age: NumberType()
     });
 
-    expect(() => mapType.validate({ name: 'the name', age: 123, foo: 'bar' })).toThrowError('expected exact keys');
+    expect(() => mapType.validate({ name: 'the name', age: 123, foo: 'bar' })).toThrow('expected exact keys');
   });
 
   it('Optional', () => {
@@ -105,18 +105,21 @@ describe('contract tests', () => {
     });
 
     expect(() => MinMaxNumberType(10, 20).validate(15)).not.toThrow();
-    expect(() => MinMaxNumberType(10, 20).validate(8)).toThrowError(`8 out of range`);
-    expect(() => MinMaxNumberType(10, 20).validate(22)).toThrowError(`22 out of range`);
+    expect(() => MinMaxNumberType(10, 20).validate(8)).toThrow(`8 out of range`);
+    expect(() => MinMaxNumberType(10, 20).validate(22)).toThrow(`22 out of range`);
     expect(() => MinMaxNumberType(10, 20).validate('hi')).toThrow();
     expect(() => MinMaxNumberType(10, 20).validate()).toThrow();
   });
 
   it('EnumType', () => {
-    expect(() => EnumType('foo', 'bar').validate()).toThrowError('expected one of foo,bar');
+    expect(() => EnumType('foo', 'bar').validate()).toThrow('expected one of foo,bar');
     expect(() => EnumType('foo', 'bar').validate('foo')).not.toThrow();
     expect(() => EnumType('foo', 'bar').validate('bar')).not.toThrow();
-    expect(() => EnumType('foo', 'bar').validate('bAr')).toThrowError('expected one of foo,bar');
-    expect(() => EnumType('foo', 'bar').validate('FOO')).toThrowError('expected one of foo,bar');
+    expect(() => EnumType('foo', 'bar').validate('bAr')).toThrow('expected one of foo,bar');
+    expect(() => EnumType('foo', 'bar').validate('FOO')).toThrow('expected one of foo,bar');
+    expect(() => EnumType(1, 3).validate(2)).toThrow('expected one of 1,3');
+    expect(() => EnumType(1, 3).validate(3)).not.toThrow();
+    expect(() => EnumType(1, 3).validate('1')).toThrow('expected one of 1,3');
   });
 });
 
